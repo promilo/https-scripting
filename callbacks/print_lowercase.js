f4d0647d19df05bf55dfcdb51660b5f6725c85db
promilo <milind.shah@mail.utoronto.ca>
2017-05-08 16:26:40 +0000

    Finished adding lowercase.js

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,18 @@
+var https = require('https');
+var http = require('http');
+var url = require('url');
+
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printLowerCase (html) {
+
+  /* Write your code here! */
+  console.log(html.toLowerCase())
+}
+
+getHTML(requestOptions, printLowerCase);

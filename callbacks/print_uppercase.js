951230a2e9c723de1af91f8991c6fb6978c92058
promilo <milind.shah@mail.utoronto.ca>
2017-05-08 16:23:07 +0000

    Finished uppercase

---
+++ callbacks/print_uppercase.js
@@ -0,0 +1,21 @@
+var https = require('https');
+var http = require('http');
+var url = require('url');
+
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUpperCase (html) {
+
+  /* Write your code here! */
+  console.log(html.toUpperCase());
+
+
+
+}
+
+getHTML(requestOptions, printUpperCase);
